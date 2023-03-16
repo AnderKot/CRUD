c40b20658393f1b18e93f262b16f110ca8a0056e
Ander_kot <85591562+AnderKot@users.noreply.github.com>
2023-03-16 21:49:24 +0300

    Подсветка выделенной строки, и при наведении курсора

--- ASP_MVC/ASP_MVC/bin/Release/net6.0/publish/wwwroot/js/site.js
+++ ASP_MVC/ASP_MVC/bin/Release/net6.0/publish/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var CurrRow = null;
+
+function ClickOnLine(row) {
+    
+
+    if (CurrRow == null) {
+        row.className = "SelectedRow";
+        CurrRow = row;
+    }
+    else {
+        if (row.className == "SelectedRow") {
+            row.className = "";
+            CurrRow = null;
+        }
+    }
+
+}
